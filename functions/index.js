687005e250cdcf967f18c88cffaf26cfb7e7224e
Vadim <ya.lishinskiy2017@gmail.com>
2020-02-05 23:30:11 +0200

    get+post+ try to make auth

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,94 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
+const Config = {
+    apiKey: "AIzaSyDT4HXHLMbly0ZIa6DVsgKu0K8wDv4vLsM",
+    authDomain: "notes-for-teachers.firebaseapp.com",
+    databaseURL: "https://notes-for-teachers.firebaseio.com",
+    projectId: "notes-for-teachers",
+    storageBucket: "notes-for-teachers.appspot.com",
+    messagingSenderId: "988993098854",
+    appId: "1:988993098854:web:a1f8e9af8a4319f9d309a7",
+    measurementId: "G-W7CY866JXL"
+};
 
-const express = require('express');
-const app = express();
+const firebase = require('firebase');
+firebase.initializeApp(Config);
 
-app.get('/screams',(req,res) => {
-    admin
-        .firestore()
-        .collection('screams')
-        .get()
-        .then((data)=>{
-            let screams =[];
-            data.forEach((doc)=>{
-                screams.push(doc.data());
-            });
-            return res.json(screams);
-        })
-        .catch((err)=> console.error(err))
+const db =admin.firestore();
+
+app.get('/screams', (req,res) =>{
+   db
+       .collection('screams')
+       .orderBy('createdAt','desc')
+       .get()
+       .then((data) =>{
+           let screams =[];
+           data.forEach((doc) =>{
+               screams.push({
+                   screamId: doc.id,
+                   body: doc.data().body,
+                   userHandle: doc.data().userHandle,
+                   createdAt: doc.data().createdAt
+               });
+           });
+           return res.json(screams);
+       })
+       .catch((err) => console.error(err));
 });
 
 
+app.post('/scream',(req,res) =>{
+   const newScream = {
+       body: req.body.body,
+       userHandle: req.body.userHandle,
+       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+   };
 
-/*the code to push info to firestore*/
-app.post('/scream',(req, res) =>{
-
-    const  newScream ={
-        body: req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+   db
+       .collection('screams')
+       .add(newScream)
+       .then(doc =>{
+           res.json({ message: 'document ' + doc.id +' created successfully'});
+       })
+       .catch(err=>{
+           res.status(500).json({ error: "something went wrong"});
+           console.error(err);
+       })
+});
+// Signup route
+app.post('/signup',(req,res) =>{
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        passwordCongig: req.body.passwordCongig,
+        handel: req.body.handel,
     };
 
-    admin.firestore()
-        .collection('screams')
-        .add(newScream)
+    db.doc('/users/'+newUser.handel).get()
         .then(doc =>{
-            res.json({message:'document ' + doc.id + ' created successfully'})
+           if(doc.exists){
+               return res.status(400).json({handle:'this hendle is already taken'});
+           }else{
+               firebase
+           }
+        });
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data =>{
+            return res.status(201).json({message: 'user '+ data.user.uid+' signed up successfully'});
         })
         .catch(err =>{
-            res.status(500).json({error : 'something went wrong'});
             console.error(err);
+            return res.status(500).json({ error: err.code});
         })
 });
+
 
 
 // https://basurl.com/api
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
 
