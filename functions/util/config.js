3ce3059f96b43474e3069b2cdaad4f488e690602
Vadim <ya.lishinskiy2017@gmail.com>
2020-02-07 22:15:42 +0200

    refactored successfuly!

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyDT4HXHLMbly0ZIa6DVsgKu0K8wDv4vLsM",
+    authDomain: "notes-for-teachers.firebaseapp.com",
+    databaseURL: "https://notes-for-teachers.firebaseio.com",
+    projectId: "notes-for-teachers",
+    storageBucket: "notes-for-teachers.appspot.com",
+    messagingSenderId: "988993098854",
+    appId: "1:988993098854:web:a1f8e9af8a4319f9d309a7",
+    measurementId: "G-W7CY866JXL"
+};
+
